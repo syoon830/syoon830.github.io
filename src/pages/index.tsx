258da2891e9a66a589a961a6fabc3b2ca5ce511a
yoon <syoon830@gmail.com>
2020-11-27 22:24:13 +0900

    build 에러에 따른 pages/styled.ts 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import {Link, graphql} from "gatsby"
 import {PageProps} from "gatsby"
 import Layout from "../components/layout";
-import {List, ListItem, Title, Date} from "./styled";
+import styled from "styled-components";
 
 type DataType = {
   allContentfulBlogPost: {
@@ -48,4 +48,27 @@ export const pageQuery = graphql`
             }
         }
     }
+`
+
+
+
+const List = styled.ul``
+const ListItem = styled.li`
+  a {
+    display: block;
+    text-decoration: none;
+    color: #333;
+    padding: 20px;
+    border-bottom: 1px solid #d6d6d6;
+  }
+`
+const Title = styled.span`
+  display: block;
+  font-size: 22px;
+  padding-bottom: 10px;
+`
+const Date = styled.span`
+  display: block;
+  font-size: 16px;
+  text-align: right;
 `
